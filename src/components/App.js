53b553213b8321ab12a6500b50e70c8d6c6d89b6
whoinlee <whoinlee@gmail.com>
2017-03-16 01:08:22 -0400

    Added SASS

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../stylesheets/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>A Sample Data Visualization w. Nike+ API data built in React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Update the code in <code>src/App.js</code>
         </p>
       </div>
     );
